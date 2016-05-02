f132320398addd3bc0e9e07078c214b321d37a05
kdorichev <kdorichev@gmail.com>
2016-05-02 03:47:58 +0300

    Initial commit

---
+++ count-emails.py
@@ -0,0 +1,13 @@
+fname = raw_input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+
+for line in fh:
+	words = line.split()
+	if not words: continue
+	if not words[0] == 'From' : continue
+	print words[1]
+	count = count + 1
+print "There were", count, "lines in the file with From as the first word"
